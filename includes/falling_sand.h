efe0ec65e30125988473bdc052724b6a8ed46316
Pierre Anger <panger@z4r2p1.42lyon.fr>
2024-01-24 15:51:58 +0100

    headerfile

--- includes/falling_sand.h
+++ includes/falling_sand.h
@@ -6,7 +6,7 @@
 /*   By: panger <panger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 15:29:15 by panger            #+#    #+#             */
-/*   Updated: 2024/01/24 15:29:47 by panger           ###   ########.fr       */
+/*   Updated: 2024/01/24 15:51:43 by panger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,30 @@
 # define FALLING_SAND_H
 
 # include <unistd.h>
+# include <mlx.h>
+
+typedef char vartype;
+
+typedef struct s_img_vars
+{
+	void	*addr;
+	int		pixel_bits;
+	int		line_bytes;
+	int		endian;
+	char	*buffer;
+}			t_img_vars;
+
+typedef struct s_vars
+{
+	void		*mlx;
+	void		*win;
+	t_img_vars	*img;
+}				t_vars;
+
+typedef struct s_param
+{
+	t_vars		*vars;
+	vartype		**grid;
+}			t_param;
 
 #endif
